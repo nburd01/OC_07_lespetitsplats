0d469cc9f304e0f7a72a229b060c41eed8fa2a13
Niall BURDON <niall.burdon@rubrash.com>
2024-01-17 18:52:19 +0100

    front ready, need minor adjustements

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,32 @@ module.exports = {
     "./index.html",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        hero: "url('../../src/assets/images/home.jpeg')",
+      },
+    },
+    // colors: {
+    //   customYellow: "#FFD15B",
+    // },
+    fontSize: {
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+      "2xl": "1.563rem",
+      "3xl": "2.5rem",
+      "4xl": "2.441rem",
+      "5xl": "3.052rem",
+    },
+    fontFamily: {
+      sans: ["ui-sans-serif", "system-ui"],
+      serif: ["ui-serif", "Georgia"],
+      mono: ["ui-monospace", "SFMono-Regular"],
+      display: ["Oswald"],
+      body: ['"Open Sans"'],
+      anton: ['"Anton"', "sans-serif"],
+      manrope: ['"Manrope"', "sans-serif"],
+    },
   },
   plugins: [],
 };
