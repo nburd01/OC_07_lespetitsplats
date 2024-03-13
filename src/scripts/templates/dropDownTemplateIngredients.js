c85b6e1b8fe9d1e8593d2d5001fc94420f35cc21
Niall BURDON <niall.burdon@rubrash.com>
2024-03-13 14:59:39 +0100

    refacto : dropdown factories displaying correct info, need to delete doubles

--- src/scripts/templates/dropDownTemplateIngredients.js
+++ src/scripts/templates/dropDownTemplateIngredients.js
@@ -5,17 +5,25 @@ class dropDownTemplateIngredients {
 
   createDropDown() {
     const sortIngredients = document.getElementById("ingredientsGo");
-
+    const appendedIngredients = new Set();
+    const ingrArray = [];
     this._card._ingredients.forEach((ingredient) => {
-      //li
-      const ingredientItem = document.createElement("a");
-      ingredientItem.classList.add("sortIngredients");
+      ingrArray.push(ingredient.ingredient);
+      console.log("ingredient", ingredient.ingredient);
+      if (!appendedIngredients.has(ingredient.ingredient)) {
+        // console.log(1);
+        //li
+        const ingredientItem = document.createElement("a");
+        ingredientItem.classList.add("sortIngredients");
 
-      //innerHTML
-      ingredientItem.innerHTML = `${ingredient.ingredient}`;
+        //innerHTML
+        ingredientItem.innerHTML = `${ingredient.ingredient}`;
 
-      //append
-      sortIngredients.appendChild(ingredientItem);
+        //append
+        sortIngredients.appendChild(ingredientItem);
+        //appendedIngr
+        appendedIngredients.add(ingredient.ingredient);
+      }
     });
 
     return sortIngredients;
